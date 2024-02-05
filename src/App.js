f81dce8b12c57858396f66dce763459434e18bbb
vicfirth@1997 <yashmundra1997@gmail.com>
2024-02-05 19:27:40 +0530

    todo with search , add and delete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
+import {useState,useRef} from 'react';
 import './App.css';
 
 function App() {
+  const [inputVal,setInputVal]=useState('');
+  const [id,setId]=useState(1);
+  const [data,setData]=useState([{id:null,value:''}]);
+  const [dataCopy,setDataCopy]=useState([{id:null,value:''}]);
+  const inputRef=useRef();
+  function handleChange(e) {
+    if(e.target.value==='') {
+      setData(dataCopy);
+    }
+    setInputVal(e.target.value);
+  }
+  function handleClick() {
+    setData((prev)=>{
+      return [...prev,{id:id,value:inputVal}]
+    });
+    setDataCopy((prev)=>{
+      return [...prev,{id:id,value:inputVal}]
+    })
+    setId(prev=>prev+1);
+    setInputVal('')
+  }
+  function handleSearch(e) {
+    const typedValue=inputRef.current.value;
+    setData(data.filter((d)=>{
+      return d.value.toLocaleLowerCase().includes(typedValue);
+    }))
+
+  }
+
+  function handleDelete(id) {
+    setData(data.filter((d)=>{
+      return d.id!==id;
+    }));
+    setDataCopy(data.filter((d)=>{
+      return d.id!==id;
+    }));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main_container'>
+        <div className='input_container'>
+          <input type='text' value={inputVal} onChange={handleChange} ref={inputRef}/>
+          <button onClick={handleClick}>Add</button>
+          <button onClick={handleSearch}>Search</button>
+        </div>
+        {data
+        .map((d)=>{
+          return (
+            <div key={d.id} className='container'>
+              <h2>{d.value}</h2>
+              {d.value && <div className='btn'><button onClick={()=>handleDelete(d.id)}>Delete</button></div>}
+            </div>
+          )
+        })}
     </div>
   );
 }
